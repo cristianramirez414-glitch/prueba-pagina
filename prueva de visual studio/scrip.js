825f425db65a0973549021ae96a9b0d85adefb1d
CRISTIAN DAVID RAMIREZ LOPEZ <cristian.ramirez414@pascualbravo.edu.co>
2026-03-11 21:20:14 -0500

    Add files via upload

---
+++ prueva de visual studio/scrip.js
@@ -0,0 +1,8 @@
+function mostrarImagen() {
+    document.getElementById("imagen").style.display = "block";
+}
+
+
+function mostrarTexto() {
+    document.getElementById("texto").style.display = "block";
+}
